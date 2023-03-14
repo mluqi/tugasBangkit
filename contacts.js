b53bcb44447d7822a612ae00905b1c174a0825b9
Mohammad Luqi <75027604+mluqi@users.noreply.github.com>
2023-03-14 17:53:15 +0700

    Add files via upload

---
+++ contacts.js
@@ -0,0 +1,17 @@
+const contacts = [
+  {
+    id: 1,
+    name: 'mluqi',
+    email: 'mluqi@example.com',
+    phone: "1234567890",
+  },
+  {
+    id: 1,
+    name: 'John Doe',
+    email: 'john@example.com',
+    phone: "1234567890",
+  },
+];
+
+
+module.exports = contacts;
